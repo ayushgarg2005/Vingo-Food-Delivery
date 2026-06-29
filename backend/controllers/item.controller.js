822039756ab11333de0b08905637e5c113b8b772
Ayush Garg <asdgarg0729@gmail.com>
2026-06-29 11:57:08 +0530

    Fix import path casing for cloudinary utility in item controller

--- backend/controllers/item.controller.js
+++ backend/controllers/item.controller.js
@@ -1,6 +1,6 @@
 import Item from "../models/item.model.js";
 import Shop from "../models/shop.model.js";
-import uploadOnCloudinary from "../utils/cloudinary.js";
+import uploadOnCloudinary from "../Utils/cloudinary.js";
 
 export const addItem = async (req, res) => {
   try {
